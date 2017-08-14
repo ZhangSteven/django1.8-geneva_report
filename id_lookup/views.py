577e79fbac9cb7fc29936df7b2b05ac8eaf991c3
Steven Zhang <steven.shutao@gmail.com>
2017-08-14 16:11:53 +0800

    using django logging now, one log file per app

--- id_lookup/views.py
+++ id_lookup/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Security
-from .utility import logger
 import json
+import logging
 
 # to make certain views exempt from security protection
 from django.views.decorators.csrf import csrf_exempt
+
+# Get an instance of a logger
+logger = logging.getLogger('app_id_lookup')
 
 
 
@@ -25,11 +28,14 @@ def security_lookup(request):
 	"""
 	Find a security based on the security_id_type and security_id
 	"""
+	logger.info('security_lookup(): enter')
+
 	if request.method == 'GET':
 		security_id_type = request.GET.get('security_id_type', '')
 		security_id = request.GET.get('security_id', '')
 		logger.debug('security_lookup() get: security_id_type={0}, security_id={1}'.\
 						format(security_id_type, security_id))
+
 		if security_id_type == '' or security_id == '':
 			logger.error('security_lookup(): invalid security_id_type or security_id')
 			raise Http404()
@@ -38,6 +44,7 @@ def security_lookup(request):
 			s = Security.objects.get(security_id_type=security_id_type,\
 										security_id=security_id)
 			return HttpResponse(s.to_json(), content_type='application/json')
+
 		except:
 			logger.exception('security_lookup()')
 			raise Http404()
